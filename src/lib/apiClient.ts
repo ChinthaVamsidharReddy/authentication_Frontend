6223479c75e55e6425c43be298c838907e37fcef
Vamsidhar Reddy <vamsidharreddy831@gmail.com>
2026-04-22 15:39:13 +0530

    minor

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError, InternalAxiosRequestConfig } from 'axios'
 import Cookies from 'js-cookie'
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080/api'
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://authentication-backend-qqf8.onrender.com/api'
 
 export const apiClient = axios.create({
   baseURL: API_URL,
